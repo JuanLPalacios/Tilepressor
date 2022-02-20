b9490fa28b5ec217c0b4cff1df9450596aa534bb
JuanLP <juan.luis.palacios.p@gmail.com>
2022-02-20 18:55:48 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+const path = require('path');
+module.exports = {
+  entry: {
+    'app': './src/main.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname)
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /.tsx?$/, loader: 'ts-loader' },
+      { test: /\.js$/, use: ['source-map-loader'], enforce: 'pre' }
+    ]
+  },
+  externals: { apprun: 'apprun', marked: 'marked' },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: __dirname,
+    },
+    compress: true,
+  },
+};
